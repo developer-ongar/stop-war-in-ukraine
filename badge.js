0cd8bb0d72b32b820b1fd8aee7a6ffc1f799a01b
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-03-17 17:54:13 +0600

    Update badge.js

--- badge.js
+++ badge.js
@@ -21,3 +21,25 @@ SWU__DOMReady(function() {
       window.open(url, '_blank')
     })
   }
+  
+  // Set custom position (left, right)
+  if (position === 'right') {
+    el.style.right = '-80px'
+    el.style.bottom = '20px'
+    el.style.transform = 'rotate(135deg)'
+    el.style.background = 'linear-gradient(-360deg, #005BBB 50%, #FFD500 50%)'
+  } else {
+    el.style.left = '-80px'
+    el.style.bottom = '20px'
+    el.style.transform = 'rotate(45deg)'
+    el.style.background = 'linear-gradient(-180deg, #005BBB 50%, #FFD500 50%)'
+  }
+
+  el.style.width = '300px'
+  el.style.height = '84px'
+  el.style.position = 'fixed';
+  el.style.zIndex = '999';
+
+  el.setAttribute('id', 'we-stand-with-ukraine')
+  document.body.appendChild(el);
+});
