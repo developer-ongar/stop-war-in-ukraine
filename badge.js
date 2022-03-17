382dbc4b00a1486abb9fe7e44c2688c6311e3d88
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2022-03-17 17:53:09 +0600

    Update badge.js

--- badge.js
+++ badge.js
@@ -2,3 +2,5 @@ var SWU__currentScript = document.currentScript || {};
 var SWU__DOMReady = function(callback) {
   document.readyState === 'interactive' || document.readyState === 'complete' ? callback() : document.addEventListener('DOMContentLoaded', callback);
 };
+SWU__DOMReady(function() {
+  const el = document.createElement('div');
